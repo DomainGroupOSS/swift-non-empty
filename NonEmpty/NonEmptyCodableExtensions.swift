71c9d09127c6846b82d549127025dd214c16e2b8
Sam Warner <sam@goro-des.co.uk>
2019-03-05 17:39:30 +1100

    placeholder for codable extensions

---
+++ NonEmpty/NonEmptyCodableExtensions.swift
@@ -0,0 +1,12 @@
+//
+//  NonEmptyCodableExtensions.swift
+//  NonEmpty
+//
+//  Created by Sam Warner on 5/3/19.
+//  Copyright © 2019 Domain Holdings Ltd. All rights reserved.
+//
+
+import Foundation
+
+// TODO: Swift 5
+//extension NonEmpty: Codable where Contained: Codable {}
